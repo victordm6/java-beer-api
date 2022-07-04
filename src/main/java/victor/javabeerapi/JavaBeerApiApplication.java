e7f4b68c5d71eee68d3cd9674f0f07bc350556e8
victordm6 <victor.dm6@hotmail.com>
2022-07-04 13:51:56 -0300

    :tada: Commit inicial

---
+++ src/main/java/victor/javabeerapi/JavaBeerApiApplication.java
@@ -0,0 +1,13 @@
+package victor.javabeerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaBeerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaBeerApiApplication.class, args);
+	}
+
+}
