8b919485ccdc4e810846cecb6a0374b720cbc541
victordm6 <victor.dm6@hotmail.com>
2022-07-06 21:01:48 -0300

    :tada: Commit inicial

---
+++ src/main/java/victor/javabeerapi/BeerMapper.java
@@ -0,0 +1,15 @@
+package victor.javabeerapi;
+
+
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface BeerMapper {
+
+    BeerMapper INSTANCE = Mappers.getMapper(BeerMapper.class);
+
+    Beer toModel(BeerDTO beerDTO);
+
+    BeerDTO toDTO(Beer beer);
+}
---
+++ src/main/java/victor/javabeerapi/BeerRepository.java
@@ -0,0 +1,10 @@
+package victor.javabeerapi;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface BeerRepository extends JpaRepository<Beer, Long> {
+
+    Optional<Beer> findByName(String name);
+}
